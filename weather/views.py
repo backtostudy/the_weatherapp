520b0aa0a3e55dd0f4be66f5ecc00db63452ada6
backtostudy <48354352+backtostudy@users.noreply.github.com>
2019-03-09 11:46:32 +0530

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -4,19 +4,12 @@ from .models import City
 from .forms import CityForm
 
 def index(request):
-    cities = City.objects.all()
-
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_APP_KEY'
-
+    cities = City.objects.all() #return all the cities in the database
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=452e9814ae5b7619469642550ebbdf2b'
     if request.method == 'POST': # only true if form is submitted
         form = CityForm(request.POST) # add actual request data to form for processing
         form.save() # will validate and save if validate
-
-        form = cityform()
-
-        weather_data = []
-
-
+    form = CityForm()
     weather_data = []
 
     for city in cities:
@@ -31,7 +24,7 @@ def index(request):
         }
 
         weather_data.append(weather) #add the data for the current city into our list
-
-    context = {'weather_data' : weather_data}
     context = {'weather_data' : weather_data, 'form' : form}
-    return render(request, 'weather/index.html')
+    context = {'weather_data' : weather_data}
+
+    return render(request, 'weather/index.html') #returns the index.html template
